b2be2887bb280866879c96cc8383af13e5828127
Hal <hgrforrest@gmail.com>
2021-11-22 14:48:16 -0500

    practicing node :comet:

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 #!/usr/bin/env node
 
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type': 'text/plain'})
-	res.end('I am the only thing here')
+// const server = http.createServer((req, res) => {
+// 	res.writeHead(200, {'Content-Type': 'text/plain'})
+// 	res.end('I am the only thing here')
+// })
+
+// server.listen(7000)
+
+// const open = require('open')
+// open('http://127.0.0.1:7000')
+
+// setTimeout(() => {
+
+// 	// process.exit()
+// }, 2000)
+
+// process.exitCode = 1
+
+const express = require('express')
+const { Server } = require('http')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('Hi!')
 })
 
-server.listen(7000)
+app.listen(3000, () => console.log('server read'))
 
-const open = require('open')
-open('http://127.0.0.1:7000')
+process.on('SIGTERM', () => {
+	Server.close(() => {
+		console.log('Process terminated');
+	})
+})
+
+
+setTimeout(() => {
+	console.log('This process is your pid ' + process.pid);
+	process.kill(process.pid, 'SIGTERM')
+}, 3000)
